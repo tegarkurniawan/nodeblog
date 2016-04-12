ce53b64cc8bf443debf71d7b371e355bb1aff318
tegar kurniawan <tegarkurniawan145@gmail.com>
2016-04-12 11:27:22 +0700

    node blog

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -1,4 +1,27 @@
 module.exports.index = function (req, res) {
 
-	res.render('index');
+	req.getConnection(function (err, connection) {
+
+		var query = connection.query('SELECT artikel.*,category.name FROM artikel left join category on category.id = artikel.category_id', function (err, rows) {
+
+			var query = connection.query('SELECT * FROM category', function (err2, rows2) {
+
+				if (err) {
+					
+					console.log("Error : %s ", err);
+					
+					}
+				if (err2) {
+					
+					console.log("Error : %s ", err2);
+					
+					}
+
+				res.render('index',{artikels: rows, categorys: rows2 });
+
+			});
+
+		});
+
+	});
 };
--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -1,5 +1,19 @@
 module.exports.index = function (req, res) {
+	req.getConnection(function (err, connection) {
 
-	res.render('login', {tagline: "Login page", message: req.flash('loginMessage') });
+		var query = connection.query('SELECT * FROM category', function (err2, rows2) {
+			
+			if (err2) {
+					
+					console.log("Error : %s ", err2);
+					
+					}
+
+			res.render('login', {tagline: "Login page", message: req.flash('loginMessage'), categorys:rows2 });
+
+		});
+
+	});
+	
 
 };
